20d24eb072a8f93e589ab3b764cbd2cb64287e22
kennedybabu <kennedynjogu8@gmail.com>
2022-07-18 11:06:26 +0300

    add font Inter from google fonts

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import {ImLocation} from "react-icons/im"
 
 const Card = (props) => {
   return (
-    <div>
-        <div>
-            {/* <img src="" alt="" /> */}
-            image
+    <div className='h-[168px] w-[470px] bg-slate-300 flex mx-auto'>
+        <div className='h-full w-[125px] rounded-[5px]'>
+            <img src={props.place.imageUrl} alt="" className='h-full rounded-[5px]'/>
         </div>
-        <div>
-            <h2>{props.place.title}</h2>
+        <div className='pl-[19px]'>
+            <div className='flex items-center'>
+                <ImLocation className='text-[#F55A5A] mr-2' />
+                <p className='mr-2'>{props.place.location}</p>
+                <a className="text-sm text-gray-500 underline" target="_blank" href={`${props.place.googleMapsUrl}`}>View on Google Maps</a>
+            </div>
+            <h1 className='text-bold text-2xl my-2'>{props.place.title}</h1>
         </div>
     </div>
   )
